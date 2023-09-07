125f05d54fceded832589fba5388538cc7b497c8
Emma Baka <112560476+emmabaka@users.noreply.github.com>
2023-09-07 15:51:33 +0300

    feat: added hero section

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,10 +9,10 @@ const Header = () => {
           <img src={logo} alt='Logo' width={104} height={26} />
         </a>
         <div className={s.buttonsWrapper}>
-          <a className={`${s.headerButtons} button`} href='#users'>
+          <a className={`${s.headerButton} button`} href='#users'>
             Users
           </a>
-          <a className={`${s.headerButtons} button`} href='#'>
+          <a className={`${s.headerButton} button`} href='#sign-up'>
             Sign up
           </a>
         </div>
---
+++ src/components/HeroSection/HeroSection.tsx
@@ -0,0 +1,23 @@
+import s from './HeroSection.module.scss';
+
+const HeroSection = () => {
+  return (
+    <section className={s.heroSection}>
+      <div className={`${s.heroContainer} container`}>
+        <h1 className={s.title}>Test assignment for front-end developer</h1>
+        <p className={s.description}>
+          What defines a good front-end developer is one that has skilled
+          knowledge of HTML, CSS, JS with a vast understanding of User design
+          thinking as they'll be building web interfaces with accessibility in
+          mind. They should also be excited to learn, as the world of Front-End
+          Development keeps evolving.
+        </p>
+        <a className={`${s.signUpButton} button`} href='#sign-up'>
+          Sign up
+        </a>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
