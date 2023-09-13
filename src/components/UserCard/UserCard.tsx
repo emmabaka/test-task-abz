3add9da2e28486e93e67b9763acd19ad8c3a2855
Emma Baka <112560476+emmabaka@users.noreply.github.com>
2023-09-13 11:16:41 +0300

    feat: added tooltips to user information

--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -21,12 +21,14 @@ const UserCard = ({
           currentTarget.src = coverPhoto;
         }}
       />
-      <p className={`${s.userInfo} ${s.userName}`} title={name}>
+      <p className={`${s.userInfo} ${s.userName}`}>
         {sliceText(name)}
+        {name.length > 25 && <span className={s.tooltip}>{name}</span>}
       </p>
       <p className={s.userInfo}>{sliceText(position)}</p>
-      <p className={s.userInfo} title={email}>
+      <p className={`${s.userInfo} ${s.userEmail}`}>
         {sliceText(email)}
+        {email.length > 25 && <span className={s.tooltip}>{email}</span>}
       </p>
       <p className={s.userInfo}>{phone}</p>
     </li>
